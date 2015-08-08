61bbdc7120cebad2f5b2fbf81e7a5b958cc23005
Petros <snoopy@sphinx.local>
2015-08-09 01:19:34 +0200

    Conflicts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -69,91 +69,91 @@ alias ve='vim ~/.vimrc'
 alias ze='vim ~/.zshrc'
 alias zr='source ~/.zshrc'
 
-# Git Aliases
-alias gs='git status'
-alias gstsh='git stash'
-alias gst='git stash'
-alias gsp='git stash pop'
-alias gsa='git stash apply'
-alias gsh='git show'
-alias gshw='git show'
-alias gshow='git show'
-alias gi='vim .gitignore'
-alias gcm='git ci -m'
-alias gcim='git ci -m'
-alias gci='git ci'
-alias gco='git co'
-alias gcp='git cp'
-alias ga='git add -A'
-alias gap='git add -p'
-alias guns='git unstage'
-alias gunc='git uncommit'
-alias gm='git merge'
-alias gms='git merge --squash'
-alias gam='git amend --reset-author'
-alias grv='git remote -v'
-alias grr='git remote rm'
-alias grad='git remote add'
-alias gr='git rebase'
-alias gra='git rebase --abort'
-alias ggrc='git rebase --continue'
-alias gbi='git rebase --interactive'
-alias gl='git l'
-alias glg='git l'
-alias glog='git l'
-alias co='git co'
-alias gf='git fetch'
-alias gfch='git fetch'
-alias gd='git diff'
-alias gb='git b'
-alias gbd='git b -D -w'
-# Staged and cached are the same thing
-alias gdc='git diff --cached -w'
-alias gds='git diff --staged -w'
-alias gpub='grb publish'
-alias gtr='grb track'
-alias gpl='git pull'
-alias gplr='git pull --rebase'
-alias gps='git push'
-alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
-alias gnb='git nb' # new branch aka checkout -b
-alias grs='git reset'
-alias grsh='git reset --hard'
-alias gcln='git clean'
-alias gclndf='git clean -df'
-alias gclndfx='git clean -dfx'
-alias gsm='git submodule'
-alias gsmi='git submodule init'
-alias gsmu='git submodule update'
-alias gt='git t'
-alias gbg='git bisect good'
-alias gbb='git bisect bad'
+# # Git Aliases
+# alias gs='git status'
+# alias gstsh='git stash'
+# alias gst='git stash'
+# alias gsp='git stash pop'
+# alias gsa='git stash apply'
+# alias gsh='git show'
+# alias gshw='git show'
+# alias gshow='git show'
+# alias gi='vim .gitignore'
+# alias gcm='git ci -m'
+# alias gcim='git ci -m'
+# alias gci='git ci'
+# alias gco='git co'
+# alias gcp='git cp'
+# alias ga='git add -A'
+# alias gap='git add -p'
+# alias guns='git unstage'
+# alias gunc='git uncommit'
+# alias gm='git merge'
+# alias gms='git merge --squash'
+# alias gam='git amend --reset-author'
+# alias grv='git remote -v'
+# alias grr='git remote rm'
+# alias grad='git remote add'
+# alias gr='git rebase'
+# alias gra='git rebase --abort'
+# alias ggrc='git rebase --continue'
+# alias gbi='git rebase --interactive'
+# alias gl='git l'
+# alias glg='git l'
+# alias glog='git l'
+# alias co='git co'
+# alias gf='git fetch'
+# alias gfch='git fetch'
+# alias gd='git diff'
+# alias gb='git b'
+# alias gbd='git b -D -w'
+# # Staged and cached are the same thing
+# alias gdc='git diff --cached -w'
+# alias gds='git diff --staged -w'
+# alias gpub='grb publish'
+# alias gtr='grb track'
+# alias gpl='git pull'
+# alias gplr='git pull --rebase'
+# alias gps='git push'
+# alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
+# alias gnb='git nb' # new branch aka checkout -b
+# alias grs='git reset'
+# alias grsh='git reset --hard'
+# alias gcln='git clean'
+# alias gclndf='git clean -df'
+# alias gclndfx='git clean -dfx'
+# alias gsm='git submodule'
+# alias gsmi='git submodule init'
+# alias gsmu='git submodule update'
+# alias gt='git t'
+# alias gbg='git bisect good'
+# alias gbb='git bisect bad'
 
-# Common shell functions
-alias less='less -r'
-alias tf='tail -f'
-alias l='less'
-alias lh='ls -alt | head' # see the last modified files
-alias screen='TERM=screen screen'
-alias cl='clear'
+# # Common shell functions
+# alias less='less -r'
+# alias tf='tail -f'
+# alias l='less'
+# alias lh='ls -alt | head' # see the last modified files
+# alias screen='TERM=screen screen'
+# alias cl='clear'
 
 # Zippin
 alias gz='tar -zcvf'
 
 # Ruby
-alias c='rails c' # Rails 3
-alias co='script/console' # Rails 2
-alias cod='script/console --debugger'
-alias ts='thin start'
-alias ms='mongrel_rails start'
-alias tfdl='tail -f log/development.log'
-alias tftl='tail -f log/test.log'
+# alias c='rails c' # Rails 3
+# alias co='script/console' # Rails 2
+# alias cod='script/console --debugger'
+# alias ts='thin start'
+# alias ms='mongrel_rails start'
+# alias tfdl='tail -f log/development.log'
+# alias tftl='tail -f log/test.log'
 
 alias ka9='killall -9'
 alias k9='kill -9'
 
 # Gem install
-alias sgi='sudo gem install --no-ri --no-rdoc'
+# alias sgi='sudo gem install --no-ri --no-rdoc'
 
 # TODOS
 # This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
@@ -163,8 +163,8 @@ alias todo='open nvalt://find/todo'
 # Forward port 80 to 3000
 alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
 
-alias rdm='rake db:migrate'
-alias rdmr='rake db:migrate:redo'
+# alias rdm='rake db:migrate'
+# alias rdmr='rake db:migrate:redo'
 
 # Zeus
 alias zs='zeus server'
@@ -180,22 +180,22 @@ alias srdt='spring rake db:migrate'
 alias srdmt='spring rake db:migrate db:test:prepare'
 
 # Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
+# alias sp='sprintly'
 # spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+# alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
 
-alias hpr='hub pull-request'
+# alias hpr='hub pull-request'
 alias grb='git recent-branches'
 
 # Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
-alias dbtp='spring rake db:test:prepare'
-alias dbm='spring rake db:migrate'
-alias dbmr='spring rake db:migrate:redo'
-alias dbmd='spring rake db:migrate:down'
-alias dbmu='spring rake db:migrate:up'
+# alias dbtp='spring rake db:test:prepare'
+# alias dbm='spring rake db:migrate'
+# alias dbmr='spring rake db:migrate:redo'
+# alias dbmd='spring rake db:migrate:down'
+# alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+# alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
